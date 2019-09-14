69bb0bc8a0f2b4bcd651420bc76b9f46f58746dc
Enragedtoast <edwardsjtimothy@gmail.com>
2019-09-14 18:51:53 -0500

    removed stock react things

--- models/stats.js
+++ models/stats.js
@@ -5,7 +5,9 @@ const playerStats = new Schema({
   player: { type: String, required: true },
   faction: { type: String, required: true },
   sub: { type: String, required: true },
-  victory: {type: Boolean, required: true},
+  victory: {type: Boolean, required: false},
+  wins: {type: Number, required: false},
+  loses: {type: Number, required: false },
   date: { type: Date, default: Date.now }
 });
 
---
+++ scripts/seedDB.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/reactwarhammerstats"
+);
+
+const statsSeed = [
+  {
+    player: "Tim",
+    faction: "Chaos",
+    sub: "Thousand Sons",
+    wins: 3,
+    loses: 7,
+  },
+  {
+    player: "Alexa",
+    faction: "Aeldari",
+    sub: "Ulthwe",
+    wins: 7,
+    loses: 3,
+  },
+  {
+    player: "John",
+    faction: "Imperium",
+    sub: "Ultramarines",
+    wins: 8,
+    loses: 2,
+  },
+  {
+    player: "Asa",
+    faction: "Tau",
+    sub: "Tau Sept",
+    wins: 6,
+    loses: 4,
+  }
+];
+
+db.playerStats
+  .remove({})
+  .then(() => db.playerStats.collection.insertMany(statsSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
