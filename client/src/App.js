38bb8ed03764bfe38a8534b57a988867ef5dd79d
Enragedtoast <edwardsjtimothy@gmail.com>
2019-09-14 19:05:41 -0500

    page background functional

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Nav from "./components/Nav"
 
 function App() {
   return (
     <div className="App">
-  
+      <Nav />
+      <h1>Hello World</h1>
     </div>
   );
 }
