a14933f5bd74b7ba691f498cccf0e20f018695d2
Enragedtoast <edwardsjtimothy@gmail.com>
2019-09-14 19:10:48 -0500

    nav bar added

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      <h1>Hello World</h1>
     </div>
   );
 }
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,10 +1,15 @@
 import React from "react"
 import "./navbar.css"
 
-export default function index() {
+export default function Navbar() {
     return (
-        <div>
-            
-        </div>
+        <>
+            <nav class="navbar navbar-dark bg-dark">
+                <a class="navbar-brand" href="#">
+                    <img src="https://www.warhammer-community.com/wp-content/plugins/gw-brand-banner/assets/images/White-Warhammerlogo.png" width="30" height="30" class="d-inline-block align-top" alt="" />
+                    Astronomicon
+                </a>
+            </nav>
+        </>
     )
 }
