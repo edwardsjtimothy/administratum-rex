d43ccf9e45f7d88207b739a5ee87a1fe80a7c4d1
Enragedtoast <edwardsjtimothy@gmail.com>
2019-09-15 23:43:28 -0500

    building body component

--- client/src/components/Body/index.js
+++ client/src/components/Body/index.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import "./body.css";
 
 export default class Body extends Component {
+
     render() {
         return (
-            <div className="body-wrap">
-                
+            <div className="body-wrap container">
+                <div className="row">
+                    <div className="col-0 col-sm-0 col-md-1 col-lg-1"></div>
+                    <div className="col-12 col-sm-12 col-md-10 col-lg-10">
+                        <ul className="nav nav-tabs justify-content-end">
+                        <h1 className="left-align">Tim</h1>
+                            <li className="nav-item">
+                                <a className="nav-link active" href="#">Active</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Link</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Link</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-0 col-sm-0 col-md-1 col-lg-1"></div>
+                </div>
             </div>
         )
     }
