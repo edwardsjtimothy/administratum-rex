a03400f40329dbdcfceab02f5cde068294506c0b
Enragedtoast <edwardsjtimothy@gmail.com>
2019-09-22 23:41:44 -0500

    working on body props

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,12 @@ import "./App.css";
 import Nav from "./components/Nav";
 import Body from "./components/Body";
 
+
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Body />
+      <Nav user="Tim" />
+      <Body  />
     </div>
   );
 }
