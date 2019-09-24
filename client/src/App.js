19e3fc577d3e96f8633a16a4e00dae7ddabe5fe9
Enragedtoast <edwardsjtimothy@gmail.com>
2019-09-23 20:15:00 -0500

    removed superfluous code

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import Nav from "./components/Nav";
 import Body from "./components/Body";
 
-
 function App() {
   return (
     <div className="App">
--- client/src/components/Body/index.js
+++ client/src/components/Body/index.js
@@ -1,49 +1,14 @@
-// import React, { Component } from 'react'
-
-
-// export default class Body extends Component {
-
-//     render() {
-//         return (
-//             <div className="body-wrap container">
-//                 <div className="row">
-//                     <div className="col-0 col-sm-0 col-md-1 col-lg-1"></div>
-//                     <div className="col-12 col-sm-12 col-md-10 col-lg-10">
-//                         <div className="sub-nav">
-//                             <h1 className="nav-user">{this.props.user}</h1>
-//                             <div className="nav-tabs">
-//                                 <button className="nav-btn btn">stuff</button>
-//                                 <button className="nav-btn btn">things</button>
-//                                 <button className="nav-btn btn">extra</button>
-//                             </div>
-//                         </div>
-//                         <div className="clear-float"></div>
-//                         <div className="body-con">
-//                             <div>
-//                                 <p>stats go here</p>
-//                             </div>
-//                         </div>
-//                     </div>
-//                     <div className="col-0 col-sm-0 col-md-1 col-lg-1"></div>
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
-
 import React from 'react'
 import "./body.css";
 
-
 export default function index(props) {
     return (
-        <>
             <div className="body-wrap container">
                 <div className="row">
                     <div className="col-0 col-sm-0 col-md-1 col-lg-1"></div>
                     <div className="col-12 col-sm-12 col-md-10 col-lg-10">
                         <div className="sub-nav">
-                            <h1 className="nav-user">{props.children}</h1>
+                            <h1 className="nav-user">{props.user}</h1>
                             <div className="nav-tabs">
                                 <button className="nav-btn btn">stuff</button>
                                 <button className="nav-btn btn">things</button>
@@ -54,14 +19,13 @@ export default function index(props) {
                         <div className="body-con">
                             <div>
                                 <p>stats go here</p>
-                                
+
                             </div>
                         </div>
                     </div>
                     <div className="col-0 col-sm-0 col-md-1 col-lg-1"></div>
                 </div>
             </div>
-        </>
     )
 }
 
