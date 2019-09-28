590e0d952afb0c2ec262b911ffc2c0772d1af5eb
Enragedtoast <edwardsjtimothy@gmail.com>
2019-09-28 10:59:15 -0500

    working on form selects

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -2,44 +2,64 @@ import React from "react"
 import "./form.css";
 
 export default function index() {
+
+    const selectListData = {
+
+        "subfaction": {
+            imp: {
+                text: ["Astra Militarum", "White Scars", "Ultramarines", "Salamanders", "Imperial Fists", "Iron Hands", "Space Wolves", "Blood Angels", "Death Watch" , "Grey Knights", "Adeptus Custodes"],
+                value: ["Mil", "WScars", "Ultra", "Sala", "IFists", "IHands", "SWolves", "BAngels", "DWatch", "GKnights", "Cus"]
+            },
+            cha: {
+                text: ["Daemons of Nurgle", "Daemons of Khorne", "Daemons of Slaanesh", "Daemons of Tzeentch", "Thousand Sons", "Death Guard", "Emperor's Children","Black Legion", "World Eaters"],
+                value: ["Nurgle","Khorne","Slaanesh","Tzeentch","TSons","DGuard","EChildren","BLegion","WEaters"]
+            },
+            xen: {
+                text: ["Aeldari", "Necrons", "Genestealer Cults", "Tyranids", "Tau", "Orks", "Harlequins", "Dark Eldar"],
+                value: ["Eld", "Nec", "Gene", "Tyr", "Tau", "Orks", "Harl", "DEld"]
+            },
+        }
+    }
+
+
+
+
     return (
         <div className="new-record">
             <form>
-                <div class="form-group row">
-                    <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Email</label>
-                    <div class="col-sm-10">
-                        <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                <div className="form-group row">
+                    <h5 className="form-heading col-sm-2">Faction</h5>
+                    <div className="select-con col-sm-10">
+                        <select className="custom-select mr-sm-2">
+                            <option selected>Choose...</option>
+                            <option value="imp">Imperium</option>
+                            <option value="cha">Chaos</option>
+                            <option value="xen">Xenos</option>
+                        </select>
+                    </div>
+                    <h5 className="form-heading col-sm-2">Subfaction</h5>
+                    <div className="select-con col-sm-10">
+                        <select name="subfaction" className="custom-select mr-sm-2">
                             <option selected>Choose...</option>
                             <option value="1">One</option>
                             <option value="2">Two</option>
                             <option value="3">Three</option>
                         </select>
                     </div>
-                </div>
-                <div class="form-group row">
-                    <label for="colFormLabel" class="col-sm-2 col-form-label">Email</label>
-                    <div class="col-sm-10">
-                        <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                    <h5 className="form-heading col-sm-2">Outcome</h5>
+                    <div className="select-con col-sm-10">
+                        <select className="custom-select mr-sm-2">
                             <option selected>Choose...</option>
                             <option value="1">One</option>
                             <option value="2">Two</option>
                             <option value="3">Three</option>
                         </select>
                     </div>
-                </div>
-                <div class="form-group row">
-                    <label for="colFormLabelLg" class="col-sm-2 col-form-label col-form-label-lg">Email</label>
-                    <div class="col-sm-10">
-                        <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-                            <option selected>Choose...</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </select>
+                    <div className="form-submit">
+                    <button className="btn btn-warning">Submit</button>
                     </div>
                 </div>
             </form>
-
         </div>
     )
 }
