cb5d38c85ef1dc9db37519a2415d049152c7eb93
Enragedtoast <edwardsjtimothy@gmail.com>
2019-10-02 12:30:00 -0500

    conditional selects working

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 import "./form.css";
 import { Select } from 'antd';
 
-const factionData = ["Imperial", "Chaos", "Xenons"]
+const { Option } = Select;
+const factionData = ["Imperial", "Chaos", "Xenos"];
 
-const factionData = {
+const subData = {
 
     Imperial: ["Astra Militarum", "White Scars", "Ultramarines", "Salamanders", "Imperial Fists", "Iron Hands", "Space Wolves", "Blood Angels", "Death Watch" , "Grey Knights", "Adeptus Custodes"],
     Chaos: ["Daemons of Nurgle", "Daemons of Khorne", "Daemons of Slaanesh", "Daemons of Tzeentch", "Thousand Sons", "Death Guard", "Emperor's Children","Black Legion", "World Eaters"],
@@ -16,45 +17,68 @@ const factionData = {
     export default class index extends Component {
 
         state = {
-            faction: factionData[]
-            subfaction:
-        }
+            faction: subData[factionData[0]],
+            subfaction: subData[factionData[0]][0],
+        };
+
+        handleFactionChange = value => {
+            this.setState({
+              faction: subData[value],
+              subfaction: subData[value][0],
+            });
+          };
+
+          onSubChange = value => {
+            this.setState({
+              subfaction: value,
+            });
+          };
+
 
         render() {
+            const { factions } = this.state;
             return (
                 <div className="new-record">
-            <form id="game-form">
-                <div className="form-group row">
-                    <h5 className="form-heading col-sm-2">Faction</h5>
-                    <div className="select-con col-sm-10">
-                        <select name="faction" className="custom-select mr-sm-2">
-                            <option selected>Choose...</option>
-                            <option value="imp">Imperium</option>
-                            <option value="cha">Chaos</option>
-                            <option value="xen">Xenos</option>
-                        </select>
-                    </div>
-                    <h5 className="form-heading col-sm-2">Subfaction</h5>
-                    <div className="select-con col-sm-10">
-                        <select name="subfaction" id="subfaction" className="custom-select mr-sm-2">
-                            <option selected>Choose...</option>
-                            
-                        </select>
-                    </div>
-                    <h5 className="form-heading col-sm-2">Outcome</h5>
-                    <div className="select-con col-sm-10">
-                        <select className="custom-select">
-                            <option selected>Choose...</option>
-                            <option value="win">I won</option>
-                            <option value="lose">I lost</option>
-                        </select>
-                    </div>
-                    <div className="form-submit">
-                    <button className="btn btn-warning">Submit</button>
-                    </div>
+                    <form id="game-form">
+                        <div className="form-group row">
+                            <h5 className="form-heading col-sm-2">Faction</h5>
+                            <div className="select-con col-sm-10">
+                                <Select
+                                    defaultValue={factionData[0]}
+                                    style={{ width: 120 }}
+                                    onChange={this.handleFactionChange}
+                                >
+                                    {factionData.map(faction => (
+                                        <Option key={faction}>{faction}</Option>
+                                    ))}
+                                </Select>
+                            </div>
+                            <h5 className="form-heading col-sm-2">Subfaction</h5>
+                            <div className="select-con col-sm-10">
+                                <Select
+                                    style={{ width: 120 }}
+                                    value={this.state.subfaction}
+                                    onChange={this.onSubChange}
+                                >
+                                    {this.state.faction.map(sub => (
+                                        <Option key={sub}>{sub}</Option>
+                                    ))}
+                                </Select>
+                            </div>
+                            <h5 className="form-heading col-sm-2">Outcome</h5>
+                            <div className="select-con col-sm-10">
+                                <select className="custom-select">
+                                    <option selected>Choose...</option>
+                                    <option value="win">I won</option>
+                                    <option value="lose">I lost</option>
+                                </select>
+                            </div>
+                            <div className="form-submit">
+                                <button className="btn btn-warning">Submit</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-            </form>
-        </div>
             )
         }
     }
